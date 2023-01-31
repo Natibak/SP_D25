cc765dd16ab08cd4c7dea698d3a1212f03b679bd
Krzysztof Szokalski <szokalskikrzysiek@gmail.com>
2023-01-31 11:56:13 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# This is student assesement Day 25
+
+It's DAY 25 task for python students
---
+++ main.py
@@ -0,0 +1,5 @@
+import pandas as pd
+import glob
+
+filepaths = glob.glob("animals/*.txt")
+print(filepaths)
